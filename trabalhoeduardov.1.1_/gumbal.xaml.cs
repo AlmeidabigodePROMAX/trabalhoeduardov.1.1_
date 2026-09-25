09159b044c0ff962192f04e80841cad957ed78c1
Mr Lima <felipebr4095@gmail.com>
2026-09-25 11:43:51 -0300

    bvb

---
+++ trabalhoeduardov.1.1_/gumbal.xaml.cs
@@ -0,0 +1,9 @@
+namespace trabalhoeduardov._1._1_;
+
+public partial class gumbal : ContentPage
+{
+	public gumbal()
+	{
+		InitializeComponent();
+	}
+}
---
+++ trabalhoeduardov.1.1_/stevenn.xaml.cs
@@ -0,0 +1,9 @@
+namespace trabalhoeduardov._1._1_;
+
+public partial class stevenn : ContentPage
+{
+	public stevenn()
+	{
+		InitializeComponent();
+	}
+}
